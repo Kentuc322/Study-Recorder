241ca32fb1bb7bf8a4a4c4ad8f4fcd600a7fc4cc
Kentuc322 <kenichirowada0322@gmail.com>
2026-06-08 11:28:36 +0900

    Supabaseを導入

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 export const SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://tqepafgcgijqbykcisur.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZXBhZmdjZ2lqcWJ5a2Npc3VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1ODI2NDEsImV4cCI6MjA5NjE1ODY0MX0.CwX_7R8KVA0mXGhmwlheqlhL3BGAmGPGO4Yr_du2cL4",
   dailyGoalMinutes: 120
 };
